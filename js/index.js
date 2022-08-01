60bf5f11a63312856d385bb840b0c3c9cdbe4d88
Axel Estrada <estradaaxel021@gmail.com>
2022-08-01 01:03:28 -0300

    agregue mas paginas a git

---
+++ js/index.js
@@ -0,0 +1,54 @@
+window.sr = ScrollReveal();
+
+
+    sr.reveal('.subrayado',{
+        duration: 1400,
+        origin: 'bottom',
+        distance: '-100px'
+
+});
+
+
+    sr.reveal('.titulo-foto',{
+        duration: 1200,
+        origin: 'top',
+        distance: '-100px'
+
+});
+
+sr.reveal('.drop-anterior',{
+    duration: 1500,
+    origin: 'left',
+    distance: '400px'
+
+});
+
+sr.reveal('.drop-anterior1',{
+    duration: 1500,
+    origin: 'right',
+    distance: '400px'
+
+});
+
+sr.reveal('.drop-anterior4',{
+    duration: 1500,
+    origin: 'bottom',
+    distance: '400px'
+
+});
+
+
+sr.reveal('.texts',{
+    duration: 1400,
+    origin: 'left',
+    distance: '400px'
+
+});
+
+
+sr.reveal('.galeria',{
+    duration: 2000,
+    origin: 'bottom',
+    distance: '400px'
+
+});
